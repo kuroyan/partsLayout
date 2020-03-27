ff357fee5fd230819a547470fc0a43778b8477da
kuroyan <m_kurosaki@mtf.biglobe.ne.jp>
2020-03-27 16:43:05 +0900

    first commit

---
+++ app/src/main/java/jp/lgs/partslayoutsample/MainActivity.kt
@@ -0,0 +1,17 @@
+package jp.lgs.partslayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickBtn(view: View){
+        //finish()
+    }
+}
