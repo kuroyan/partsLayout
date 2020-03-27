496ef5e8fc086e8dafde223e0d195860142ddef2
kuroyan <m_kurosaki@mtf.biglobe.ne.jp>
2020-03-27 16:55:00 +0900

    second commit

--- app/src/main/java/jp/lgs/partslayoutsample/MainActivity.kt
+++ app/src/main/java/jp/lgs/partslayoutsample/MainActivity.kt
@@ -2,7 +2,10 @@ package jp.lgs.partslayoutsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickBtn(view: View){
-        //finish()
+        var switcha = switch1
+        if(switcha.isChecked) {
+            toastMake("Switch ON",0,200)
+        }
+        else {
+            toastMake("Switch OFF",0,200)
+        }
     }
+
+    private fun toastMake(message: String, x: Int, y: Int) {
+        val toast = Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT)
+        // 位置調整
+        toast.setGravity(Gravity.CENTER, x, y)
+        toast.show()
+    }
+
+
 }
